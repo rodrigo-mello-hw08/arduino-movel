89963b74551a8da5c6ab7da1a103e8d5fb17c9f0
Rodrigo Mello <hollowrm08@gmail.com>
2025-11-04 21:59:56 -0300

    Change sensor reading from analog to digital

--- codigo-debug-infra.ino.ino
+++ codigo-debug-infra.ino.ino
@@ -1,19 +1,32 @@
+
+
+/*
+A5 = sensor linha esquerdo  400
+A4 = sensor linha direito  400
+A3 = determinar interrupcao 
+A2 = determinar interrupcao
+A1 = sensor qtd linha 800
+A0 = sensor proximidade bloco 430
+*/
+
+
 void setup() {
   Serial.begin(9600);
-  pinMode(A2, INPUT);
+  pinMode(A0, INPUT);
 }
 
+
 void loop() {
-  int iPot = analogRead(A2);
-  int iMap = map(iPot, 0, 1023, 0, 10);
+  int iPot = digitalRead(A0);
+  // int iMap = map(iPot, 0, 1023, 0, 10);
   
   Serial.print("iPot: ");
   Serial.println(iPot);
-  Serial.print("mapeamento: ");
-  Serial.println(mapEmCimaDaLinha(iPot));
+  // Serial.print("mapeamento: ");
+  // Serial.println(mapEmCimaDaLinha(iPot));
   delay(200);
 }
 
 int mapEmCimaDaLinha(int entrada) {
-	return entrada >= 450 ? 0 : 1;
+	return entrada >= 750 ? 0 : 1;
 }
